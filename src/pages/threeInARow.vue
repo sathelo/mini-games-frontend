6b0f164c200ba4b26d6c8810d115dfa7e123603a
sathelo <sathelo@yandex.ru>
2024-01-09 17:48:18 +0300

    feat: Добавил скелеты 3-х мини игр

---
+++ src/pages/threeInARow.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>this is threeInARow</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import BaseLayout from "@/layouts/default.vue";
 import ErrorLayout from "@/layouts/error.vue";
 import HomePage from "@/pages/index.vue";
+import TicTacToePage from "@/pages/ticTacToe.vue";
+import SeaBattlePage from "@/pages/seaBattle.vue";
+import ThreeInARowPage from "@/pages/threeInARow.vue";
 
 import { ERoutes } from "@/router/routes.types";
 
@@ -19,17 +22,17 @@ const routes = [
       {
         path: "",
         name: ERoutes.ticTacToe,
-        component: HomePage,
+        component: TicTacToePage,
       },
       {
         path: "",
         name: ERoutes.seaBattle,
-        component: HomePage,
+        component: SeaBattlePage,
       },
       {
         path: "",
         name: ERoutes.threeInARow,
-        component: HomePage,
+        component: ThreeInARowPage,
       },
     ],
   },
